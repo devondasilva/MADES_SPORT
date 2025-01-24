2a80a080ddcbafe0cb748f64475f967061e93bd7
devondasilva <dasilvadevon51@gmail.com>
2025-01-24 11:19:56 +0100

    Mise a jour des editions de formations

--- src/pages/Formations/BeachTennis/Editions/edition3.tsx
+++ src/pages/Formations/BeachTennis/Editions/edition3.tsx
@@ -1,20 +1,21 @@
 import Newsletter from "../../../../components/email/newsletter";
 import Foot from "../../../../components/footer/foot/foot";
-import Banner from "../../../../components/formation/banner/banner";
 import Gallery from "../../../../components/formation/gallery/gallery";
 import Header from "../../../../components/Header/header";
-
-import box from "../../../assets/Images/ballsboxAsset 6.png";
+import im6 from "../../../../assets/Images/im6.png";
 
 export default function Edition3() {
   return (
     <>
-      <Header />
-      <Banner
-        title="IMAGES BEACH TENNIS"
-        imageSrc={box}
-        optionalText="EDITION 2024 "
-      />
+      <Header headertitle="BEACH TENNIS" headerimage={im6}>
+        <p>
+          <strong>MADES</strong> est une organisation dynamique dédiée à la
+          promotion du sport et de l'excellence dans les domaines du tennis, du
+          CrossFit, et bien plus encore. Nous accompagnons les entraîneurs dans
+          leur parcours professionnel en proposant des programmes de formation
+          innovants et des outils adaptés aux besoins actuels du marché.
+        </p>
+      </Header>
       <Gallery />
       <Newsletter />
       <Foot />
