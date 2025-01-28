d4dd766ebdb1db63b13a231913d3e95e0dfd3898
devondasilva <dasilvadevon51@gmail.com>
2025-01-28 08:36:49 +0100

    Modification du formulaire

---
+++ src/components/caroussel.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import { Paper, Button } from '@mui/material';
+
+interface ItemProps {
+    item: {
+        name: string;
+        description: string;
+        imageUrl: string;
+    };
+}
+
+// Removed unused ExampleProps interface
+
+function Example() {
+    const items: ItemProps['item'][] = [
+        {
+            name: "Random Name #1",
+            description: "Probably the most random thing you have ever seen!",
+            imageUrl: "https://via.placeholder.com/400x300"
+        },
+        {
+            name: "Random Name #2",
+            description: "Hello World!",
+            imageUrl: "https://via.placeholder.com/400x300"
+        }
+    ];
+
+    return (
+        <Carousel>
+            {
+                items.map((item, i) => <Item key={i} item={item} />)
+            }
+        </Carousel>
+    );
+}
+
+function Item(props: ItemProps) {
+    return (
+        <Paper>
+            <img src={props.item.imageUrl} alt={props.item.name} style={{ width: '100%', height: 'auto' }} />
+            <h2>{props.item.name}</h2>
+            <p>{props.item.description}</p>
+            <Button className="CheckButton">
+                Check it out!
+            </Button>
+        </Paper>
+    );
+}
+
+export default Example;
